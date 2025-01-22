9361a5ac19d3f10b6234367a276c58207062200a
SergioMDLT <smonterodelatorre@hotmail.com>
2025-01-22 12:56:04 +0100

    feature add search by capital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    HttpClientModule,
     SharedModule
   ],
   providers: [],
--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -7,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './by-capital-page.component.css'
 })
 export class ByCapitalPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor( private countriesService: CountriesService ) { }
+
+  searchByCapital( term: string ) {
+    this.countriesService.searchCapital( term )
+      .subscribe( countries => {
+        this.countries = countries;
+      });
+  }
 
 }
