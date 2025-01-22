82bd88b9bc81306dece71163d5cf3ea49ea8feb4
SergioMDLT <smonterodelatorre@hotmail.com>
2025-01-22 09:20:22 +0100

    feature homeComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    HomePageComponent
+  ],
+  exports: [
+    HomePageComponent,
   ]
 })
 export class SharedModule { }
