21e09c397f9ff12a10b09ea1163a7b7ab140467b
SergioMDLT <smonterodelatorre@hotmail.com>
2025-01-22 13:58:12 +0100

    feature add search by id

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, catchError } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -9,6 +10,15 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
   constructor(private http: HttpClient) { }
+
+  searchCountryByAlphaCode( code: string ): Observable<Country | null> {
+    const url = `${ this.apiUrl }/alpha/${ code }`;
+    return this.http.get<Country[]>( url )
+      .pipe(
+        map( countries => countries.length > 0 ? countries[0]: null),
+        catchError( ()  => of(null) )
+      );
+  }
 
   searchCapital( term: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/capital/${ term }`;
