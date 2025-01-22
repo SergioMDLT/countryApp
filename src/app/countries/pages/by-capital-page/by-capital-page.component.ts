bb58b03fbb9de4c52fd6aab2e0689857da2cd4a8
SergioMDLT <smonterodelatorre@hotmail.com>
2025-01-22 10:55:14 +0100

    feature add filtered search

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital-page',
+  standalone: false,
+  templateUrl: './by-capital-page.component.html',
+  styleUrl: './by-capital-page.component.css'
+})
+export class ByCapitalPageComponent {
+
+}
