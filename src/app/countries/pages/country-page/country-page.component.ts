7f080001a323d7b1c3df918606cc9b60973058fc
SergioMDLT <smonterodelatorre@hotmail.com>
2025-01-23 08:38:51 +0100

    feature add info page

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap, tap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -10,6 +11,8 @@ import { switchMap, tap } from 'rxjs';
   styleUrl: './country-page.component.css'
 })
 export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -25,13 +28,8 @@ export class CountryPageComponent implements OnInit {
         switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode( id ))
       )
       .subscribe(  country  => {
-
-        if ( !country ) {
-          return this.router.navigateByUrl('');
-        }
-
-        console.log('TENEMOS UN PAIS');
-        return;
+        if ( !country ) return this.router.navigateByUrl('');
+        return this.country = country;
       })
   }
 
